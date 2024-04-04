ef27a95eea402e027654b1d0f1780518e8af3dbf
ciiska5 <zayacvpole34@gmail.com>
2024-04-04 17:05:22 +0300

    Create template for project

---
+++ src/main/java/ru/masterDetail/docsAndPositions/DocsAndPositionsApplication.java
@@ -0,0 +1,13 @@
+package ru.masterDetail.docsAndPositions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsAndPositionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsAndPositionsApplication.class, args);
+	}
+
+}
